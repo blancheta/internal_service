099836d744b7dfddec630b4995ca6f395578053b
aldazar <alexandreblanchet44@gmail.com>
2025-07-25 16:00:44 +0100

    fix: add comments in config

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 
-DATA_LOADER = "local_service"
+DATA_LOADER = "local_service"  # can be service or local_service
+# local_service will use cached data and service will get data directly
+# from data provider
